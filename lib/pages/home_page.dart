3f6cc49c6595770a1f38421c7b7e254b7aba3ebb
SandroMaglione <lass.maglio@gmail.com>
2023-05-31 19:49:59 +0200

    show list of events in home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,38 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:fpdart_riverpod/datasources/get_all_event/get_all_event_state.dart';
+import 'package:fpdart_riverpod/providers/event_list_provider.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 @RoutePage()
-class HomePage extends StatelessWidget {
+class HomePage extends HookConsumerWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const SafeArea(child: Scaffold(body: Placeholder()));
+  Widget build(BuildContext context, WidgetRef ref) {
+    final eventList = ref.watch(eventListProvider);
+    return SafeArea(
+      child: Scaffold(
+        body: eventList.map(
+          loading: (_) => const Text('Loading...'),
+          error: (error) => Text("Error: $error"),
+          data: (list) => switch (list.value) {
+            QueryGetAllEventError() => const Text("Empty"),
+            SuccessGetAllEventState(eventEntity: final eventEntity) => Column(
+                children: [
+                  Text('${eventEntity.length} length'),
+                  ...eventEntity
+                      .map(
+                        (eventModel) => Card(
+                          child: Text(eventModel.title),
+                        ),
+                      )
+                      .toList()
+                ],
+              )
+          },
+        ),
+      ),
+    );
   }
 }
